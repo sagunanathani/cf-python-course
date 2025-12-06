7fbb9e14b41a1c3d7250f108a64f733fe70d43eb
Saguna Nathani <sagunanathani@gmail.com>
2025-12-06 17:28:08 +0100

    chore: remove Django app code from cf-python-course (moved to recipe-app repo)

--- ingredients/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create models here.
-class Ingredient(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
