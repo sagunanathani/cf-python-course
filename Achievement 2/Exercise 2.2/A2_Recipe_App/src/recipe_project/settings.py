d68d49fadae60e12dca07fef616975df7e79e91a
Saguna Nathani <sagunanathani@gmail.com>
2025-12-16 23:37:42 +0100

    Fix ALLOWED_HOSTS for Render deployment

--- Achievement 2/Exercise 2.2/A2_Recipe_App/src/recipe_project/settings.py
+++ Achievement 2/Exercise 2.2/A2_Recipe_App/src/recipe_project/settings.py
@@ -27,10 +27,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["django-recipe-app-6t2d.onrender.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
